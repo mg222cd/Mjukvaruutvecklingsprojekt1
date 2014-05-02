0fee72e1658f0843a9720e024ce8ad996cb3a7e1
Marike Grinde <mg222cd@student.lnu.se>
2014-05-02 13:29:33 +0200

    uppdaterat

---
+++ kod/wp-content/themes/marike/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+	<meta name="description" content="   	
+		Nybyggt hus i centrala Ramundberget. 50 meter till lift och slalombackar, 
+		50 m till längdspår, 50 meter till livsmedelsaffär och 100 meter till Ramundbergets centrum.">
+	<meta name="keywords" content="Ramundberget, Ramundberget boende">
+	<meta name="author" content="Ramundboende.se">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- CSS & JS TODO: lägg in egen CSS nedan. TODO: Utvärdera på iPhonne om maximum-scale ovan behöver stå kvar.
+  ================================================== -->
+	<link rel="stylesheet" href="<?php bloginfo('stylesheets/base.css'); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheets/skeleton.css'); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheets/layout.css'); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheets/style.css'); ?>">
+	<!-- Other files, such as Javascript can be called using the bloginfo('template_url'); tag. This renders the path to the theme folder in HTML. -->
+	
+	<!-- Favicon TODO:lägg in.
+	================================================== -->
+	<link rel="shortcut icon" href="">
+	<link rel="apple-touch-icon" href="">
+<?php wp_head(); ?>
+</head>
+
+<body>
+	<div class="container">
