ebb31fd86c3097ef2f76e2998ee504cdefafeb6e
Marike Grinde <mg222cd@student.lnu.se>
2014-05-09 14:39:29 +0200

    uppdaterat kod + planering

--- kod/wp-content/plugins/ramundboende/MasterController.php
+++ kod/wp-content/plugins/ramundboende/MasterController.php
@@ -23,12 +23,13 @@ class MasterController {
 		$stugor = $wpdb->get_results( "SELECT * FROM wp_ramundboende_property" );
 		$bokningar= array();
 		foreach ($stugor as $stuga) {
-			$bokningar[] = $wpdb->get_results( "SELECT * FROM wp_ramundboende_booking WHERE CustomerId=1 AND PropertyId=".$stuga->PropertyId );	
+			$bokningar[] = $wpdb->get_results( "SELECT * FROM wp_ramundboende_booking WHERE CustomerId=1 AND PropertyId=".$stuga->PropertyId." ORDER BY Year, Week" );	
 		}
 		$output ="";
 		for ($i=0; $i < count($stugor); $i++) { 
 			$output.= $this->renderTable($stugor[$i], $bokningar[$i]);
 		}
+		$output.=$this->newCustomerForm();
 		return $output;
 	}
 	
@@ -48,7 +49,7 @@ class MasterController {
 			$table.= '<td class="table_info">'.$bokningsrad->Year.'</td>';
 			$table.= '<td class="table_info">'.$bokningsrad->Week.'</td>';
 			$table.= '<td class="table_info">'.$bokningsrad->Price.'</td>';
-			$table.= '<td><input id="button" name="bookingButton" type="submit" value="Boka"></td>';
+			$table.= '<td><input type="hidden" value="'.$bokningsrad->BookingId.'"><input id="button" name="bookingButton" type="submit" value="Boka"></td>';
 			$table.= '</tr>';
 		}
 		$table.= '</table>';
@@ -57,9 +58,11 @@ class MasterController {
 	}
 	
 	public function newCustomerForm(){
-		
-		return "
-				";
+		return'<div id="overlay">
+    	This is where you will put your inline HTML for the content inside of the overlay
+		</div>
+		<div id="fade"></div>
+		';
 	}
 }
 
