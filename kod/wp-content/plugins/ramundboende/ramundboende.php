6faf543ecc68adbb26def067eb00a6b9787f23a7
Marike Grinde <mg222cd@student.lnu.se>
2014-06-04 00:14:12 +0200

    uppdaterade dokument

--- kod/wp-content/plugins/ramundboende/ramundboende.php
+++ kod/wp-content/plugins/ramundboende/ramundboende.php
@@ -48,8 +48,15 @@ class MasterController {
 			$bokningar[] = $wpdb->get_results( "SELECT * FROM wp_ramundboende_booking WHERE CustomerId=1 AND PropertyId=".$stuga->PropertyId." AND Week >= ".date('W')." OR CustomerId=1 AND PropertyId=".$stuga->PropertyId." AND Year >= ".date('Y')." ORDER BY Year, Week" );	
 		}
 		$output ="";
-		for ($i=0; $i < count($stugor); $i++) { 
-			$output.= $this->renderTable($stugor[$i], $bokningar[$i]);
+		$countBooking = 0;
+		for ($i=0; $i < count($stugor); $i++) {
+			$countBooking += count($bokningar[$i]);
+			if (count($bokningar[$i]) > 0) {
+				$output.= $this->renderTable($stugor[$i], $bokningar[$i]);
+			}	 
+		}
+		if ($countBooking < 1) {
+			$output .= '<div class="error">För tillfället är alla veckor fullbokade.</div>';
 		}
 		return $output;
 	}
@@ -124,13 +131,22 @@ class MasterController {
 		if ($submitForm[1]) {
 			//lyckad bokning
 			$h1 = "<h1>Tack för din bokning!</h1>".$h4;
-			$button = '<button type="submit" id="breakBookingbutton" name="breakBookingButton">Återgå</button>';
-			$p = "<p>Här är lite text...</p>";
+			//$button = '<button type="submit" id="breakBookingbutton" name="breakBookingButton">Återgå</button>';
+			$button = '<a class="button" href="http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'].'">Återgå</a>';
+			$p = '<p>'.$name.'<br>';
+			$p .= $address.'<br>';
+			$p .= $postal.' '.$city.'<p>';
+			$p .= '<p>Bokningsbekräftelse har skickas till '.$email.'. Får ni ingen bekräftelse, kontrollera även skräpposten.</p>';
 			$message=array($h1,$p.$button);
 		}
 		else {
-			$h1 = $h4.'<div class="red_text">'.$submitForm[0].'</div>';
-			$message = array($h1,$form);
+			if ($message = "Dubbelbokning!") {
+				$message = "det här är en dubbelbokning";
+			} 
+			else {
+				$h1 = $h4.'<div class="red_text">'.$submitForm[0].'</div>';
+				$message = array($h1,$form);	
+			}
 		}
 		return'<div id="overlay" class="'.$class.'">
 					'.$message[0].'
@@ -158,34 +174,44 @@ class MasterController {
 	
 		global $wpdb;
 		$booking = $wpdb->get_results( "SELECT * FROM wp_ramundboende_booking WHERE BookingId=".$hidden);
-		if (empty($name) || empty($address) || empty($postal) || empty($city) || empty($phone) || empty($email)) {
+		
+		//säkerhetskontroll för att undvika dubbelbokning
+		if ($booking[0]->CustomerId == 2) {
+			if (empty($name) || empty($address) || empty($postal) || empty($city) || empty($phone) || empty($email)) {
 			$message = "Inget fält får lämnas tomt.";
 			return array($message, $boolean, $booking);
-		}
-		if ($this->checkEmail($email)) {
-			$wpdb->insert( 'wp_ramundboende_customer', array('Name' => $name, 'Address' => $address, 'Postal' => $postal, 
-															'City' => $city, 'Phone' => $phone, 'Email' => $email ) );
-			$wpdb->update( 'wp_ramundboende_booking', array('CustomerId' => $wpdb->insert_id), array('BookingId' => $hidden) );	
-			$stuga = $wpdb->get_results( "SELECT * FROM wp_ramundboende_property WHERE PropertyId=".$booking[0]->PropertyId);
-			$sendinfo = array(
-				'customerInfo'=>array(
-					'Name' => $name, 
-					'Address' => $address, 
-					'Postal' => $postal, 
-					'City' => $city, 
-					'Phone' => $phone, 
-					'Email' => $email 
-				), 
-				'stuga'=>$stuga[0]->PropertyName, 
-				'week'=>$booking[0]->Week, 
-				'year'=>$booking[0]->Year, 
-				'price'=>$booking[0]->Price
-			);
-			$boolean = $this->sendToCustomer($sendinfo) && $this->sendToAdmin($sendinfo);
-			$message = "";
-		}	
+			}
+			if ($this->checkEmail($email)) {
+				$wpdb->insert( 'wp_ramundboende_customer', array('Name' => $name, 'Address' => $address, 'Postal' => $postal, 
+																'City' => $city, 'Phone' => $phone, 'Email' => $email ) );
+				$wpdb->update( 'wp_ramundboende_booking', array('CustomerId' => $wpdb->insert_id), array('BookingId' => $hidden) );	
+				$stuga = $wpdb->get_results( "SELECT * FROM wp_ramundboende_property WHERE PropertyId=".$booking[0]->PropertyId);
+				$sendinfo = array(
+					'customerInfo'=>array(
+						'Name' => $name, 
+						'Address' => $address, 
+						'Postal' => $postal, 
+						'City' => $city, 
+						'Phone' => $phone, 
+						'Email' => $email 
+					), 
+					'stuga'=>$stuga[0]->PropertyName, 
+					'stugnr'=>$stuga[0]->PropertyId,
+					'week'=>$booking[0]->Week, 
+					'year'=>$booking[0]->Year, 
+					'price'=>$booking[0]->Price
+				);
+				$boolean = $this->sendToCustomer($sendinfo) && $this->sendToAdmin($sendinfo);
+				$message = "";
+			}	
+			else {
+				
+				$message = "Ogiltigt format på E-postadressen.";
+			}
+		} 
 		else {
-			$message = "Ogiltigt format på E-postadressen.";
+			$message = "Dubbelbokning!";
+			$boolean = false;
 		}
 		return array($message, $boolean, $booking);
 	}
@@ -203,21 +229,77 @@ class MasterController {
 	}
 	
 	private function sendMail($email, $subject, $message){
-		return mail($email, $subject, $message);
+		$headers = "MIME-Version: 1.0\r\n";
+		$headers .= "Content-Type: text/HTML; charset=utf-8\r\n";
+		$headers .= 'From: info@ramundboende.se' . "\r\n" .
+    	'Reply-To: info@ramundboende.se' . "\r\n";
+		return wp_mail($email, $subject, $message, $headers);
 	}
 	
-	private function sendToCustomer($data=array('customerInfo'=>array(), 'stuga'=>'', 'week'=>'', 'year'=>'', 'price'=>'')){
+	private function sendToCustomer($data=array('customerInfo'=>array(), 'stuga'=>'', 'stugnr'=>'', 'week'=>'', 'year'=>'', 'price'=>'')){
 		$subject = "Bokningsbekräftelse";
-		$message = '<h4>Bokning av '.$data['stuga']->PropertyName.' vecka: '.$data['week']->Week.' år: '.$data['year']->Year.'. Pris: '.$data['price']->Price.':-</h4>';
-		$message .= "fritext";
+		$message = '<h1>Bokningsbekräftelse</h1>';
+		$message .= '<h2>Stuga nr '.$data[stugnr].' - '.$data['stuga'].'. Vecka: '.$data['week'].', år: '.$data['year'].'. Pris: '.$data['price'].':-'. "</h2>";
+		$message .= $data['customerInfo']['Name'].'<br>';
+		$message .= $data['customerInfo']['Address'].'<br>';
+		$message .= $data['customerInfo']['Postal'].' '.$data['customerInfo']['Postal'].'<br><br>';
+		$message .= 'Telefon: ' .$data['customerInfo']['Phone'].'<br>';
+		$message .= 'Mail: ' .$data['customerInfo']['Email'].'<br><br><br>';
+		$message .= '<p>In/utcheckningsdag under vintersäsongen före påskveckan är lördag, efter påskveckan söndag. Övriga veckor lördag.
+					Incheckning tidigast kl 15 ankomstdagen och utcheckning senast kl 11 avresedagen. Nyckeln sitter i dörren vid ankomst.
+					Adress: Klinkvägen 2, 84097 Bruksvallarna. Vägbeskrivning: 50 m efter Ramundbergets centrum, första till höger efter 
+					avfarten till Ramis Livs.</p>';
+		$message .= '<p>Bokningsavgift 1500:- betalas inom 14 dagar, resterande summa senast 30 dagar före ankomst. Bankgiro: 5903-8596 Bruksvallarnas Semesterboende AB,
+					märk betalningen med namn och veckonummer.</p>';
+		$message .= '<p>Sänglinne och handdukar ingår ej. Önskas slutstädning ska detta anmälas i förhand, pris för detta är <br>
+					Stuga 1 - Lillstugan: 900:-<br>
+					Stuga 2 - Storstugan: 2500:-</p>';
+		$message .= '<p>Hör av er vid frågor (kontaktuppgifter nedan).</p>';		
+		$message .= '<p>Välkommen till Ramundberget!</p>';
+		$message .= '<p>Bruksvallarna '.date('Y-m-d').'<br>';
+		$message .= 'Med vänlig hälsning,</p>';
+		$message .= '<p>Andreas Hoflin<br>
+					Bruksvallarnas Semesterboende AB<br>
+					Gruvarbetarvägen 10<br>
+					840 97 Bruksvallarna<br><br>
+					Mail: info@ramundboende.se<br>
+					Telefon: 070-3251974 / 070-6221065
+					</p>';		
+		$message .= '</HTML>';
 		$email = $data['customerInfo']['Email'];
 		return $this->sendMail($email, $subject, $message);
 	}
 	
-	private function sendToAdmin($data=array('customerInfo'=>array(), 'stuga'=>'', 'week'=>'', 'year'=>'', 'price'=>'')){
-		$subject = "Ny bokning!";
-		$message = '<h4>Bokning av '.$data['stuga']->PropertyName.' vecka: '.$data['week']->Week.' år: '.$data['year']->Year.'. Pris: '.$data['price']->Price.':-</h4>';
-		$message .= "";
+	private function sendToAdmin($data=array('customerInfo'=>array(), 'stuga'=>'', 'stugnr'=>'', 'week'=>'', 'year'=>'', 'price'=>'')){
+		$subject = "Bokningsbekräftelse";
+		$message = '<h1>Bokningsbekräftelse</h1>';
+		$message .= '<h2>Stuga nr '.$data[stugnr].' - '.$data['stuga'].'. Vecka: '.$data['week'].', år: '.$data['year'].'. Pris: '.$data['price'].':-'. "</h2>";
+		$message .= $data['customerInfo']['Name'].'<br>';
+		$message .= $data['customerInfo']['Address'].'<br>';
+		$message .= $data['customerInfo']['Postal'].' '.$data['customerInfo']['Postal'].'<br><br>';
+		$message .= 'Telefon: ' .$data['customerInfo']['Phone'].'<br>';
+		$message .= 'Mail: ' .$data['customerInfo']['Email'].'<br><br><br>';
+		$message .= '<p>In/utcheckningsdag under vintersäsongen före påskveckan är lördag, efter påskveckan söndag. Övriga veckor lördag.
+					Incheckning tidigast kl 15 ankomstdagen och utcheckning senast kl 11 avresedagen. Nyckeln sitter i dörren vid ankomst.
+					Adress: Klinkvägen 2, 84097 Bruksvallarna. Vägbeskrivning: 50 m efter Ramundbergets centrum, första till höger efter 
+					avfarten till Ramis Livs.</p>';
+		$message .= '<p>Bokningsavgift 1500:- betalas inom 14 dagar, resterande summa senast 30 dagar före ankomst. Bankgiro: 5903-8596 Bruksvallarnas Semesterboende AB,
+					märk betalningen med namn och veckonummer.</p>';
+		$message .= '<p>Sänglinne och handdukar ingår ej. Önskas slutstädning ska detta anmälas i förhand, pris för detta är <br>
+					Stuga 1 - Lillstugan: 900:-<br>
+					Stuga 2 - Storstugan: 2500:-</p>';
+		$message .= '<p>Hör av er vid frågor (kontaktuppgifter nedan).</p>';		
+		$message .= '<p>Välkommen till Ramundberget!</p>';
+		$message .= '<p>Bruksvallarna '.date('Y-m-d').'<br>';
+		$message .= 'Med vänlig hälsning,</p>';
+		$message .= '<p>Andreas Hoflin<br>
+					Bruksvallarnas Semesterboende AB<br>
+					Gruvarbetarvägen 10<br>
+					840 97 Bruksvallarna<br><br>
+					Mail: info@ramundboende.se<br>
+					Telefon: 070-3251974 / 070-6221065
+					</p>';		
+		$message .= '</HTML>';
 		$email = get_option('admin_email');
 		return $this->sendMail($email, $subject, $message);
 	}
